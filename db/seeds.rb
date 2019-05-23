7b793eed9254746e557d7aea54bf6ebd4ee37be3
Paulo DAlberti <phdalberti@gmail.com>
2019-05-23 15:27:00 +0900

    added models, controllers, routes, and views

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,44 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts "destroying database"
+Ingredient.destroy_all
+Cocktail.destroy_all
+Dose.destroy_all
+
+puts "creating ingredients"
+
+Ingredient.create!(name: "lemon")
+Ingredient.create!(name: "orange")
+Ingredient.create!(name: "lime")
+Ingredient.create!(name: "ice")
+Ingredient.create!(name: "orange juice")
+Ingredient.create!(name: "pinaple juice")
+Ingredient.create!(name: "coconut milk")
+Ingredient.create!(name: "milk")
+Ingredient.create!(name: "soy milk")
+Ingredient.create!(name: "apple juice")
+Ingredient.create!(name: "lime juice")
+Ingredient.create!(name: "coke")
+Ingredient.create!(name: "sprite")
+Ingredient.create!(name: "water")
+Ingredient.create!(name: "ice tea")
+Ingredient.create!(name: "syrup")
+Ingredient.create!(name: "blueberry")
+Ingredient.create!(name: "pear")
+Ingredient.create!(name: "cider")
+Ingredient.create!(name: "kiwi")
+Ingredient.create!(name: "grapes")
+Ingredient.create!(name: "almond")
+Ingredient.create!(name: "grapefruit")
+Ingredient.create!(name: "mint")
+Ingredient.create!(name: "watermelon")
+Ingredient.create!(name: "melon")
+
+puts "creating cocktails"
+Cocktail.create!(name: "trial")
+
+puts "creating doses"
+Dose.create!(description: "trial", cocktail_id: Cocktail.first.id, ingredient_id: Ingredient.first.id)
+
+puts "finished"
