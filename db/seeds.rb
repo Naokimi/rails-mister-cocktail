463ecf2546aa827409c09f23852d86bd96d73c9d
Paulo DAlberti <phdalberti@gmail.com>
2019-05-24 13:58:52 +0900

    added image support

--- db/seeds.rb
+++ db/seeds.rb
@@ -40,9 +40,24 @@ Ingredient.create!(name: "watermelon")
 Ingredient.create!(name: "melon")
 
 puts "creating cocktails"
-Cocktail.create!(name: "Virgin cuba libre")
+url = "https://d2v9y0dukr6mq2.cloudfront.net/video/thumbnail/2HqZxjJ/videoblocks-cola-with-ice-close-up-for-a-very-large-glass-of-cold-coke-with-ice-cubes-rotation_sm8lm4k7cg_thumbnail-full01.png"
+cocktail = Cocktail.new(name: "Virgin cuba libre")
+cocktail.remote_photo_url = url
+cocktail.save!
+url = "https://www.tablespoon.com/-/media/Images/Articles/Images-for-Posts-PrePandoNext/2016/12/non-alcoholic-pineapple-orange-sherbert-punch.jpg?la=en"
+cocktail = Cocktail.new(name: "Pineapple-Orange Sherbert Punch")
+cocktail.remote_photo_url = url
+cocktail.save!
+url = "https://www.tablespoon.com/-/media/Images/Articles/Images-for-Posts-PrePandoNext/2016/12/non-alcoholic-pineapple-ginger-sparkler.jpg?la=en"
+cocktail = Cocktail.new(name: "Pineapple Ginger Sparkler")
+cocktail.remote_photo_url = url
+cocktail.save!
+url = "https://www.tablespoon.com/-/media/Images/Articles/Images-for-Posts-PrePandoNext/2016/12/non-alcoholic-sangria.jpg?la=en"
+cocktail = Cocktail.new(name: "Non-Alcoholic Sangria")
+cocktail.remote_photo_url = url
+cocktail.save!
 
 puts "creating doses"
-Dose.create!(description: "all", cocktail_id: Cocktail.first.id, ingredient_id: Ingredient[11].id)
+Dose.create!(description: "all", cocktail_id: Cocktail.first.id, ingredient_id: Ingredient.find(12).id)
 
 puts "finished"
